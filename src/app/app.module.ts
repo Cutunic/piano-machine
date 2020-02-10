4880e516319c442cff03b859824d841313222ae3
Matej Cutunic <matej.cutunic@gmail.com>
2020-02-10 15:04:40 +0100

    components created; mp3 assets; service created; basic layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PianoComponent } from './components/piano/piano.component';
+import { KeypadComponent } from './components/keypad/keypad.component';
+import { KeyComponent } from './components/key/key.component';
+import { VolumeComponent } from './components/volume/volume.component';
+import { PowerComponent } from './components/power/power.component';
+import { SizeComponent } from './components/size/size.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PianoComponent,
+    KeypadComponent,
+    KeyComponent,
+    VolumeComponent,
+    PowerComponent,
+    SizeComponent
   ],
   imports: [
     BrowserModule,
